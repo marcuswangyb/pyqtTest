f12a2d7dad0929797d29b5044c7fb6c2e2d60f89
marcuswangyb <marcuswangyb@gmail.com>
2013-04-19 01:13:29 +0800

    add src

---
+++ src/pyqtTest.py
@@ -0,0 +1,16 @@
+# -*- coding: cp936 -*-
+'''
+Created on 2013-4-13
+
+@author: wangyb
+@brief:
+'''
+
+import sys
+from PyQt4 import QtGui
+
+a = QtGui.QApplication(sys.argv)
+label = QtGui.QLabel("Hello Qt!")
+label.resize(400,200)
+label.show()
+a.exec_()
